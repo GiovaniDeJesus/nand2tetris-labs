17cd2b0b6787536f2b82a52009c1ace18d8990e5
GiovaniDeJesus <135411622+GiovaniDeJesus@users.noreply.github.com>
2023-10-11 16:53:17 -0400

    Create ip.py

---
+++ ip.py
@@ -0,0 +1,36 @@
+import re
+import socket
+
+# Define regular expression patterns for IP addresses, URLs, and IP with port
+ip_pattern = r'\b(?:\d{1,3}\.){3}\d{1,3}\b'
+url_pattern = r'https?://\S+'
+ip_with_port_pattern = r'\b(?:\d{1,3}\.){3}\d{1,3}:\d{1,5}\b'
+
+# Read the list from a text file (change 'input.txt' to the actual file name)
+with open('input.txt', 'r') as file:
+    text = file.read()
+
+# Find all matches for IP addresses, URLs, and IP with port
+ip_addresses = re.findall(ip_pattern, text)
+urls = re.findall(url_pattern, text)
+ip_with_ports = re.findall(ip_with_port_pattern, text)
+
+# Resolve IP addresses or domains along with port numbers
+resolved_entries = []
+
+for entry in ip_addresses:
+    resolved_entries.append((entry, socket.gethostbyaddr(entry)[0] if '.' in entry else 'N/A'))
+
+for entry in urls:
+    hostname = entry.split('//')[1].split('/')[0]
+    resolved_entries.append((entry, socket.gethostbyname(hostname)))
+
+for entry in ip_with_ports:
+    parts = entry.split(':')
+    hostname = parts[0]
+    port = parts[1]
+    resolved_entries.append((entry, socket.gethostbyname(hostname) + ':' + port))
+
+# Print the resolved entries
+for entry, resolved in resolved_entries:
+    print(f"{entry} resolves to {resolved}")
